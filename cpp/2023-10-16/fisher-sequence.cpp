fb44a6a6f5a694e96a873acf7cf7ea56049206c1
Mirai1129 <minecraftmiku831@gmail.com>
2023-10-16 11:20:03 +0800

    recursion practice output fixed

--- cpp/2023-10-16/fisher-sequence.cpp
+++ cpp/2023-10-16/fisher-sequence.cpp
@@ -2,22 +2,28 @@
 
 using namespace std;
 
-int rabbitBorn(int);
+int RabbitBorn(int);
 
-int rabbitBorn(int months) { // Clang-Tidy: Function 'rabbitBorn' is within a recursive call chain
+int RabbitBorn(int months) { // Clang-Tidy: Function 'RabbitBorn' is within a recursive call chain
     if (months == 0) {
         return 0;
     }
     if (months == 1) {
         return 1;
     }
-    return rabbitBorn(months - 2) + rabbitBorn(months - 1);
+    return RabbitBorn(months - 2) + RabbitBorn(months - 1);
 }
 
 int main() {
     int months;
     cout << "請輸入一個數字：";
     cin >> months;
-    cout << rabbitBorn(months);
+    if (months < 0) {
+        cout << "請輸入大於等於零的數字";
+    } else {
+        for (int i = 1; i <= months; ++i) {
+            cout << "f(" << i << ")=" << RabbitBorn(i) << endl;
+        }
+    }
     return 0;
 }
