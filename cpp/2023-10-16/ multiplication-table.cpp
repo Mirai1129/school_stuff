241df55a3111246e8c3841399a47b17b28067ac6
Mirai1129 <minecraftmiku831@gmail.com>
2023-10-24 18:39:54 +0800

    new solution in cout

--- cpp/2023-10-16/ multiplication-table.cpp
+++ cpp/2023-10-16/ multiplication-table.cpp
@@ -16,6 +16,7 @@ void multiply() {
     for (int i = 1; i <= 9; i++) {
         for (int j = 1; j <= 9; j++) {
             cout << i << "*" << j << " = " << setw(2) << i * j << " ";
+            // cout << i << "*" << j << " = " << (i * j < 10 ? " ":"") << i*j << " ";
         }
         cout << endl;
     }
